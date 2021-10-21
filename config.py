065c1eaf117e144eedc2f79077f4e814cedebded
Harshal Chavan <harshal06chavan@gmail.com>
2021-10-21 07:29:07 +0000

    Upload new  File

---
+++ config.py
@@ -0,0 +1,66 @@
+
+# Maximum number of threads
+MAX_REQ = 30
+
+# Default port
+PORT = 5000
+
+# Status Codes
+statusCodes = {
+    101 : "Switching Protocols",
+    200 : "OK",
+    201 : "Created",
+    202 : "Accepted",
+    203 : "Non-Authoritative Information",
+    204 : "No Content",
+    205 : "Reset Content",
+    206 : "Partial Content",
+    300 : "Multiple Choices",
+    301 : "Moved Permanently",
+    302 : "Found",
+    303 : "See Other",
+    304 : "Not Modified",
+    305 : "Use Proxy",
+    307 : "Temporary Redirect",
+    400 : "Bad Request",
+    401 : "Unauthorized",
+    402 : "Payment Required",
+    403 : "Forbidden",
+    404 : "Not Found",
+    405 : "Method Not Allowed",
+    406 : "Not Acceptable",
+    407 : "Proxy Authentication Required",
+    408 : "Request Time-out",
+    409 : "Conflict",
+    410 : "Gone",
+    411 : "Length Required",
+    412 : "Precondition Failed",
+    413 : "Request Entity Too Large",
+    414 : "Request-URI Too Large",
+    415 : "Unsupported Media Type",
+    416 : "Requested range not satisfiable",
+    417 : "Expectation Failed",
+    500 : "Internal Server Error",
+    501 : "Not Implemented",
+    502 : "Bad Gateway",
+    503 : "Service Unavailable",
+    504 : "Gateway Time-out",
+    505 : "HTTP Version Not Supported"
+}
+
+fileFormats = {
+    "text/html" : ".html",
+    "text/css" : ".css",
+    "application/gzip" : ".gz",
+    "application/json" : ".json",
+    "application/javascript" : ".js",
+    "application/pdf" : ".pdf",
+    "appllication/octet-stream" : ".bin",
+    "image/gif" : ".gif",
+    "image/vnd.microsoft.icon" : ".ico",
+    "image/jpeg" : ".jpg",
+    "image/aviff" : ".png",
+    "audio/mpeg" : ".mp3",
+    "video/mpeg" : ".mpeg"
+}
+
