0d29022419d051751b40ccbdae9fcc45259ea846
Harshal Chavan <harshal06chavan@gmail.com>
2021-10-21 07:34:03 +0000

    Upload New File

---
+++ parse.py
@@ -0,0 +1,19 @@
+
+def parseGetReq(headers, clientAddr, rawData):
+    pass
+
+
+def parsePostReq(headers, clientAddr, rawData):
+    pass
+
+
+def parsePutReq(headers, clientAddr, rawData):
+    pass
+
+
+def parseDeleteReq(headers, clientAddr, rawData):
+    pass
+
+
+def parseHeadReq(headers, clientAddr, rawData):
+    pass
