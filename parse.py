9f0d862b533cc088a4cce9d247cb409cff983f18
Harshal Chavan <harshal06chavan@gmail.com>
2021-10-25 10:53:41 +0530

    Implemented parseHeaders function

--- parse.py
+++ parse.py
@@ -1,4 +1,30 @@
+import os
 
+def parseHeaders(data, method):
+    body = []
+    hdrValues = {}
+    if method == 'POST':
+
+        for i in data[1:]:
+            if ':' in i:
+                hdrField = i[: i.index(':')]
+                hdrValues[hdrField] = i[i.index(':') + 2 : len(i) - 1]
+            else:
+                if i != '\r' and i != '\n':
+                    body.append(i)
+        return hdrValues, body
+
+    elif method == 'PUT':
+        body = []
+        for i in data[1:]:
+            if ':' in i:
+                hdrField = i[:i.index(':')]
+                hdrValues[hdrField] = i[i.index(':') : + 2 : len(i) - 1]
+            else:
+                hdrValues['index'] = data.index(i) + 1
+                return hdrValues, body
+        return hdrValues, body
+        
 def parseGetReq(headers, clientAddr, rawData):
     pass
 
