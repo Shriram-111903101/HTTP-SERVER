4d2be69840eb3086353c9ceb14fe6483875397e6
Shriram-111903101 <fajagesa19.comp@coep.ac.in>
2021-11-13 09:38:53 +0530

    GET response added

--- response.py
+++ response.py
@@ -1,9 +1,10 @@
 import datetime
 from config import statusCodes
 from config import entityHeaders
+import random
 
-def getDateTime():
-    date = datetime.datetime.now()
+def getDateTime(offset=0):
+    date = datetime.datetime.now() + datetime.timedelta(seconds=offset)
     date = date.strftime('%A, %d %B %Y %H:%M:%S')
     date += ' GMT'
     return date
@@ -16,10 +17,41 @@ def createResponse(length, code, resource=None,
     
     date = getDateTime()
     status = "HTTP/1.1 " + str(code) + " " + statusCodes[code] + "\r\n"
-    server = "Server: myHTTP\r\n"
+    server = "Server: http-server\r\n"
     entityHed = ("Content-Type: " + contentType + "\r\nDate: " + date + 
                 "\r\nContent-Length: " + str(length) +"\r\nConnection: keep-alive\r\nAllow: " 
                 + entityHeaders['Allow'] + "\r\n\r\n")
     return status + server + entityHed
 
-#print(createResponse(30, 500))
+def cookie():
+    return ('Set-Cookie: ID = cookie' + str(random.randint(1, 1000)) + 
+            '; Expires = ' + getDateTime(15) + '; Path = /login\r\n')
+
+
+def getResponse(headers):
+    statusCode = headers['code']
+    if statusCode not in statusCodes:
+        return
+
+    date = getDateTime()
+    status = "HTTP/1.1 " + str(statusCode) + " " + statusCodes[statusCode] + "\r\n"
+    server = "Server: http-server\r\n"
+    entityHed = ("Content-Type: " + headers['cType'] + "\r\nDate: " + date + 
+                "\r\nContent-Length: " + headers['length'] + + "\r\nContent-Language: en-US\r\nConnection: keep-alive\r\nAllow: " 
+                + entityHeaders['Allow'] + "\r\n")
+
+    eTag = headers['eTag']
+    if eTag != '':
+        if 'Cookie' not in headers.keys():
+            entityHed += cookie()
+
+        entityHed += 'E-Tag: ' + eTag + '\r\n\r\n'
+    else:
+        if 'Cookie' not in headers.keys():
+            entityHed += cookie()
+    return status + server + entityHed
+
+
+
+#print(getDateTime(50))
+
